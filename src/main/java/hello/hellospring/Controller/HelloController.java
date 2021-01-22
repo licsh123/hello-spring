02ed76edbf26cadb210407cc862d2141671839c9
seugnhoon <licsh123@naver.com>
2021-01-22 18:31:50 +0900

    Initial commit

---
+++ src/main/java/hello/hellospring/Controller/HelloController.java
@@ -0,0 +1,49 @@
+package hello.hellospring.Controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data","hello!!");
+        return "hello";
+    }
+    @GetMapping("hello-mvc")
+    public String hellomvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello-template";
+    }
+    @GetMapping("hello-string")
+    @ResponseBody //http에서 헤드부와 바디부가 있음 그 바디에 이 데이터를 직접 넣어주겠다는 뜻
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name; // hello spring
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName(){
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+
+
+    }
+}
